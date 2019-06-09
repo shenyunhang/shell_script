39235e76f2c2902c5b89ba46c52fdf900bb3a593
shenyunhang <yhshen@stu.xmu.edu.cn>
2019-06-09 20:00:27 +0800

    shenyunhang-OptiPlex-3020@2019-06-09_20-00-27:

--- delete_comment_in_tex.sh
+++ delete_comment_in_tex.sh
@@ -3,4 +3,10 @@
 set -x
 
 find . -name "*.tex" -type f | xargs -I '{}' -n 1 sed -n -e '/^%/p' {}
+
+# remove whitespace at begin of line
+find . -name "*.tex" -type f | xargs -I '{}' -n 1 sed -i 's/^ *//' {}
+# remove tab at begin of line
+find . -name "*.tex" -type f | xargs -I '{}' -n 1 sed -i 's/^\t*//' {}
+# delete line begin with %
 find . -name "*.tex" -type f | xargs -I '{}' -n 1 sed -i '/^%/d' {}
