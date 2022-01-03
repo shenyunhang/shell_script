21308ab9a8d1e9731079d15378c43e6ff16dcdcb
shenyunhang <shenyunhang01@gmail.com>
2022-01-03 13:25:37 +0800

    677afa276754@2022-01-03_13-25-37:

--- tex/format_bib.sh
+++ tex/format_bib.sh
@@ -6,14 +6,14 @@ set -e
 CURRENTPATH=`pwd`
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P  )"
 
-cd ${SCRIPTPATH}
+#cd ${SCRIPTPATH}
 
-bash ${SCRIPTPATH}/remove_unused_bibtex_tag.sh ${CURRENTPATH}/${1}.bib ${1}.bib
+bash ${SCRIPTPATH}/remove_unused_bibtex_tag.sh ${1}.bib ${1}.bib
 
 bash ${SCRIPTPATH}/convert_to_full_booktitle.sh ${1}.bib
 
 bash ${SCRIPTPATH}/uc.sh $1
 
-cp ${1}U.bib ${CURRENTPATH}/${1}_format.bib
+mv ${1}U.bib ${1}_format.bib
 
-rm ${1}.bib ${1}U.bib
+#rm ${1}.bib ${1}U.bib
